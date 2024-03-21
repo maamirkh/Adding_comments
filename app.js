bda6725d0107c713102473ef057d1e503198c32c
maamirkh <maamirkhan1983@gmail.com>
2024-03-22 00:53:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+// aamir, 2024-03-05
+//this program prints a personalmessage.
+let myName = "Aamir";
+console.log(`Hello ${myName}, would you like to learn some Typescript today?`);
---
+++ app.ts
@@ -0,0 +1,4 @@
+// aamir, 2024-03-05
+//this program prints a personalmessage.
+let myName: string = "Aamir";
+console.log(`Hello ${myName}, would you like to learn some Typescript today?`);
